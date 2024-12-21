313a61428ca4d85c08544f090afb53f496dd1559
sasa <arnoa69@gmail.com>
2024-12-21 15:11:11 +0100

    only pdftron plugin for both ios&android

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,23 +1,11 @@
-
-import { createRouter, createWebHistory } from 'vue-router'
 import IndexPage from '../views/IndexPage.vue'
-import IndexPageAA from '../views/IndexPageAA.vue'
 import GolfCourseList from '../views/GolfCourseList.vue'
 
 const routes = [
   {
     path: '/',
     name: 'index',
-    component: () => {
-      // Check if it's Safari on mobile
-      const isSafariMobile = /Safari/.test(navigator.userAgent) && 
-                          //   /Mobile/.test(navigator.userAgent) && 
-                             !/Chrome/.test(navigator.userAgent)
-      
-      return new Promise((resolve) => {
-        resolve(isSafariMobile ? IndexPage : IndexPageAA);
-      })
-    }
+    component: IndexPage
   },
   {
     path: '/golf-courses',
