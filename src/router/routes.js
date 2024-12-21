95afdfebbed2b41292c9e2dde551467cbc968895
mac <mac@macs-MBP.home>
2024-12-21 12:48:56 +0100

    workes on ios with plugin pdftron, made deviation in route.js for dynamic component import

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,23 @@
+
+import { createRouter, createWebHistory } from 'vue-router'
 import IndexPage from '../views/IndexPage.vue'
+import IndexPageAA from '../views/IndexPageAA.vue'
 import GolfCourseList from '../views/GolfCourseList.vue'
 
 const routes = [
   {
     path: '/',
     name: 'index',
-    component: IndexPage
+    component: () => {
+      // Check if it's Safari on mobile
+      const isSafariMobile = /Safari/.test(navigator.userAgent) && 
+                          //   /Mobile/.test(navigator.userAgent) && 
+                             !/Chrome/.test(navigator.userAgent)
+      
+      return new Promise((resolve) => {
+        resolve(isSafariMobile ? IndexPage : IndexPageAA);
+      })
+    }
   },
   {
     path: '/golf-courses',
